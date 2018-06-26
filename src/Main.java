e490e39f6ad29abd812390e6efa08c8da84fdedf
maita <maita@192.168.160.128>
2018-06-26 18:13:28 +0900

    aaa

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,53 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println("helw");
+		double height; //身長
+		double weight; //体重
+
+		height = 172;
+		weight = 46;
+
+		System.out.println(weight/(height*height));
 
 	}
-
 }
+
+//////////////////////課題3///////////////////////////
+//	public static void main(String[] args) {
+//
+//		double r;
+//		r = 2.5;
+//		final double i = 3.14159;
+//		System.out.println(r * r * i);
+//		System.out.println((r+r)*i);
+//	}
+//}
+
+//////////////////////課題1///////////////////////////
+//	public static void main(String[] args) {
+//
+//		char a;
+//		byte b;
+//		short c;
+//		int d;
+//		float e;
+//		boolean f;
+//		String g;
+//
+//		a = 65;
+//		b = 127;
+//		c = 32767;
+//		d = 2147483647;
+//		e = 1f;
+//		f = false;
+//		g = "aaa";
+//
+//		System.out.println(a);
+//		System.out.println(b);
+//		System.out.println(c);
+//		System.out.println(d);
+//		System.out.println(e);
+//		System.out.println(f);
+//		System.out.println(g);
+//	}
+//}
