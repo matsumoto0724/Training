7cb5ef51abc2ac94c8b219f10a648302ef04ff42
maita <maita@192.168.160.128>
2018-06-28 16:28:21 +0900

    課題14

--- src/Sub11_20.java
+++ src/Sub11_20.java
@@ -1,62 +1,97 @@
 public class Sub11_20 {
 
+//////////////////////課題14///////////////////////////
+	/*内容
+	 秒単位の時間(long型)を引数で受けて、日時分秒へ変換して表示するメソッドを作成しなさい。
+	 適当な引数を指定してmainメソッドから呼び出しなさい。戻り値はありません。*/
+
+	public static void main(String[]args) {
+
+		long a = 225756;	//適当秒数
+		time(a);	//メソッド呼び出し(表示)
+
+	}
+	static void time(long a) {
+
+		long b = a/86400; //日 a/24時間(86400秒)
+		long c = a/3600; //時 a/1時間(3600秒)
+		long d = (a%3600)/60; //分 a%1時間(3600秒)の剰余を60で割る(時間に変換できる秒数を除いた秒数から分を求める)
+		long e = a%60; //秒 a/60の余りを秒
+
+//		秒→分→時間→日数で繰り上げたい
+
+//		秒→分繰り上げ
+		d = d+(e/60);	//(秒を分にした時の余り＝秒/1分(60秒))
+		e = e%60;	//余りを秒として出す
+
+//		分→時間繰り上げ
+		c = c+(d/60);	//(秒から変換した"分"/1時間(60分))
+		d = d%60;		//余りを分として出す
+
+//		時間→日数繰り上げ
+		b = b+(c/24);	//(秒から変換した"時間"/1日(24時間))=足すべき日数 → 余りは切捨て
+		c = c%24;		//(62/24)した時の余りを時間として出す
+
+		System.out.println(b+"日と"+c+"時間"+d+"分"+e+"秒");
+	}
+}
 //////////////////////課題13///////////////////////////
 /*	内容
 	2つの整数(int型)を引数で受け、その四則演算と剰余を計算して表示するメソッドを作成し
 	適当な引数を指定してmainメソッドを呼び出して、戻り値を画面に表示しなさい*/
 
-	public static void main(String[]args) {
-
-		//メソッド呼出(引数入力)
-		plus(5,5);
-		minus(10,5);
-		multiplication(2,5);
-		division(10,2);
-		rest(23,10);
-
-	}
-	/**
-	 * 四則演算をする
-	 * @param a
-	 * @param b
-	 * @author maita
-	 */
-//足し算
-	static void plus(int a, int b) {
-
-		int c = a+b;
-		System.out.println(a+"+"+b+"="+c);
-	}
-
-//	引き算
-	static void minus(int a, int b) {
-
-		int c = a-b;
-		System.out.println(a+"-"+b+"="+c);
-	}
-
-//	掛け算
-	static void multiplication(int a, int b) {
-
-		int c = a*b;
-		System.out.println(a+"×"+b+"="+c);
-	}
-
-//	割り算
-	static void division(int a, int b) {
-		int c = a/b;
-		System.out.println(a+"÷"+b+"="+c);
-	}
-
-//	剰余
-	static void rest(int a,int b) {
-
-		int c = a/b; //割
-		int d = a%b; //余
-
-		System.out.println(a+"÷"+b+"="+c +"余り"+d);
-	}
-}
+//	public static void main(String[]args) {
+//
+//		//メソッド呼出(引数入力)
+//		plus(5,5);
+//		minus(10,5);
+//		multiplication(2,5);
+//		division(10,2);
+//		rest(23,10);
+//
+//	}
+//	/**
+//	 * 四則演算をする
+//	 * @param a
+//	 * @param b
+//	 * @author maita
+//	 */
+////足し算
+//	static void plus(int a, int b) {
+//
+//		int c = a+b;
+//		System.out.println(a+"+"+b+"="+c);
+//	}
+//
+////	引き算
+//	static void minus(int a, int b) {
+//
+//		int c = a-b;
+//		System.out.println(a+"-"+b+"="+c);
+//	}
+//
+////	掛け算
+//	static void multiplication(int a, int b) {
+//
+//		int c = a*b;
+//		System.out.println(a+"×"+b+"="+c);
+//	}
+//
+////	割り算
+//	static void division(int a, int b) {
+//		int c = a/b;
+//		System.out.println(a+"÷"+b+"="+c);
+//	}
+//
+////	剰余
+//	static void rest(int a,int b) {
+//
+//		int c = a/b; //割
+//		int d = a%b; //余
+//
+//		System.out.println(a+"÷"+b+"="+c +"余り"+d);
+//	}
+//}
 //////////////////////課題12///////////////////////////
 /*	内容：
 	3つの整数を引数で受け、その平均値をdoubleの戻り値で返すメソッドになっている。
